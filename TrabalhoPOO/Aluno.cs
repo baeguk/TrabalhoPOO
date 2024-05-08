c70dc243ff2967ddaf85cbca2e2356bfc7eb3c1d
Larissa Vianna <larissa.vianna@embeddo.io>
2024-05-08 17:00:16 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoPOO/Aluno.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabalhoPOO;
+
+
+namespace TrabalhoPOO
+{
+    //Declaração de atributos
+    class Aluno
+    {
+        public int Matricula { get; set; }
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public float Nota { get; set; }
+
+        public Aluno(int matricula, string nome, int telefone, float nota)
+        {
+            Matricula = matricula;
+            Nome = nome;
+            Telefone = telefone;   
+            Nota = nota;
+            
+        }
+
+        //Exemplo de Métodos
+
+        public void Estado(float nota)
+        {
+            if ((nota >= 70) && (nota <=100))
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if ((nota >= 40) && (nota < 70))
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else if (nota < 40)
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+
+        //Declaração de Propriedades
+        /*  private int matricula;
+            private string nome;
+            private int telefone;
+         
+        public int Matricula
+        {
+            get 
+            { 
+                return matricula;
+            }
+            set { 
+                matricula = value;
+            }
+
+        }
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            { 
+                nome = value;
+            }
+        }
+        public int Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+
+            set
+            { 
+                telefone = value;
+            }
+
+        }*/
+        
+    }
+}
+
+//Exemplo de sobrecarga de Métodos
+    namespace Sobrecargas
+    {
+    //Quando temos vários métodos com o mesmo nome e parâmetros diferente, ocorre a sobrecaga
+    class Turma
+    {
+        public int Numero { get; set; }
+        public float NumeroT { get; set; }
+
+        public Turma(int numeroManha)
+        {
+            Numero = numeroManha;
+        }
+
+        public Turma(float numeroTarde)
+        {
+            NumeroT = numeroTarde;
+        }
+
+        //O comando abaixo, no C# já é definido de modo padrão
+        public Turma()
+        {
+        }
+    }
+}
+
+
+namespace Unidirecional
+    {
+    public class Professor
+    {
+        public string Nome { get; set; }
+        public string Disciplina { get; set; }
+
+        public Professor(string nome, string disciplina)
+        {
+            Nome = nome;
+            Disciplina = disciplina;
+        }
+
+        public void Informacoes()
+        {
+            Console.WriteLine($"O professor {Nome}, é responsável pela disciplina: {Disciplina} \n");
+        }
+    }
+
+    public class Aluno1
+    {
+        public string Nome { get; set; }
+        public int Turma1 { get; set; }
+        public Professor Prof { get; set; }
+
+        public Aluno1 (string nome, int turma, Professor prof)
+        {
+            Nome = nome;
+            Turma1 = turma;
+            Prof = prof;
+        }
+
+        public void Informacoes()
+        {
+            Console.WriteLine($"O aluno {Nome}, da turma: {Turma1}, tem como professor responsável: {Prof.Nome} \n");
+            Prof.Informacoes();
+        }
+      }
+
+    }
+
+    namespace Bidirecional
+    {
+        public class Disciplina
+        {
+            public string Nome { get; set; }
+            public List<Aluno2> AlunosMatriculados { get; set; }
+
+            public Disciplina(string nome)
+            {
+                Nome = nome;
+                AlunosMatriculados = new List<Aluno2>();
+            }
+
+            public void NovoAluno(Aluno2 aluno)
+            {
+                AlunosMatriculados.Add(aluno);
+            }
+
+            public void Informacoes1()
+            {
+                Console.WriteLine($"\n Na disciplina {Nome}, estão matriculados os(as) alunos(as) ");
+      
+                foreach (var aluno in AlunosMatriculados)
+                {
+                    Console.WriteLine($"{aluno.Nome}");
+                }
+            }
+        }
+
+        public class Aluno2
+        {
+            public string Nome { get; set; }
+            public List<Disciplina> Disciplinas { get; set; }
+
+            public Aluno2(string nome)
+            {
+                Nome = nome;
+                Disciplinas = new List<Disciplina>();
+            }
+
+            public void Matricular(Disciplina disciplina)
+            {
+                Disciplinas.Add(disciplina);
+                disciplina.NovoAluno(this);
+            }
+
+            public void Informacoes1()
+            {
+                Console.WriteLine($"Aluno: {Nome}, está matriculado na disciplina");
+                
+                foreach (var disciplina in Disciplinas)
+                {
+                    Console.WriteLine($" {disciplina.Nome}");
+                }
+            }
+        }
+
+    }
---
+++ TrabalhoPOO/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+using TrabalhoPOO;
+using Sobrecargas;
+using Unidirecional;
+using Bidirecional;
+
+
+namespace TrabalhoPOO
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{Atributo e Propriedade}");
+            Aluno aluno1 = new Aluno(123, "Nanda", 999141405, 80);
+            aluno1.Matricula = 123;
+            aluno1.Nome = "Ana";
+            aluno1.Telefone = 999141405;
+            aluno1.Nota = 80;
+
+            Console.WriteLine("Matrícula do aluno: "+aluno1.Matricula+"\n"+ "Nome do aluno: "
+                +aluno1.Nome+"\n"+"Telefone do aluno: "+aluno1.Telefone+"\n"+"Nota do aluno: "
+                +aluno1.Nota+"\n");
+
+            Console.WriteLine("{Método}");
+            aluno1.Estado(80);
+
+
+            Console.WriteLine("\n{Sobrecargas}");
+            Turma turma = new Turma();
+            turma.Numero = 1001;
+            turma.NumeroT = 2001;
+
+            Console.WriteLine("A sala 1 no turno da manhã pertence a turma: " + turma.Numero 
+                + ", já na parte da tarde, essa sala pertence a turma: " + turma.NumeroT+ "\n");
+
+            Console.WriteLine("{Associação Unidirecional}");
+            Professor professor = new Professor("Rosenclever", "POO");
+            
+            Aluno1 aluno = new Aluno1("Arthur", 5, professor);
+
+            aluno.Informacoes();
+
+
+            Console.WriteLine("{Associação Bidirecional}");
+                Disciplina progMob = new Disciplina("Programação Mobile");
+                Disciplina redes = new Disciplina("Redes");
+
+                Aluno2 kananda = new Aluno2("Kananda");
+                Aluno2 larissa = new Aluno2("Larissa");
+                Aluno2 maira = new Aluno2("Maíra");
+                Aluno2 sarah = new Aluno2("Sarah");
+
+                kananda.Matricular(redes);
+                larissa.Matricular(progMob);
+                maira.Matricular(progMob);
+                sarah.Matricular(redes);
+
+                kananda.Informacoes1();
+                Console.WriteLine();
+                larissa.Informacoes1();
+                Console.WriteLine();
+                maira.Informacoes1();
+                Console.WriteLine();
+                sarah.Informacoes1();
+                Console.WriteLine();
+
+                progMob.Informacoes1();
+                Console.WriteLine();
+                redes.Informacoes1();
+                Console.WriteLine();
+            }
+
+    }
+}
+    
+         
